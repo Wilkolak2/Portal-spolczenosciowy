d9bb3752ba335ce94c81b14a3567afc724da275d
Yaroslav Zarytskyi <77810796+yaroslavzarytskyi@users.noreply.github.com>
2025-03-14 14:35:47 +0100

    zrobiłem przykładowe posty i informacje w profilu

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,33 @@
 function Profile() {
-    return (
+    return (<>
         <div className="container mt-4">
             <div className="card p-4 text-center shadow">
                 <h2>Profil użytkownika</h2>
                 <p>Tu będą informacje o użytkowniku.</p>
             </div>
         </div>
+            <div className="container mt-4">
+                <div className="card p-4 shadow">
+                    <div className="container d-flex flex-row">
+                        <div className="col-8">
+                            <h3>Nazwa użytkownika</h3>
+                            <p>Imię: Jan Kowalski</p>
+                            <p>Wiek: 22</p>
+                            <p>Miasto: Wrocław</p>
+                            <p>Opis: Hej lubię placki</p>
+                            <p>Użytkownik portalu od: 14.03.2025</p>
+                        </div>
+                        <div className="col-4">
+                            <span className="rounded">
+                                <img
+                                    src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"
+                                    className="card-img-top rounded-5" alt="placeholder"/>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
